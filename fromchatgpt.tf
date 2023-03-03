92a3db84480ba637eb20641e2b17f93f475695c3
Ahmed <gomaaa447>
2023-03-03 22:01:39 +0200

    add helm provider

--- fromchatgpt.tf
+++ fromchatgpt.tf
@@ -1,21 +1,22 @@
-#    provider "helm" {
-#      kubernetes {
-#        config_path    = "~/.kube/config"
-#        config_context = "${terraform.output.kubeconfig_context}"
-#      }
-#    }
-#    
-#    data "github_repository" "free5gc" {
-#      owner = "5G-Grad-Proj"
-#      name  = "towards5gs-helm"
-#    }
-#    
-#    resource "helm_release" "free5gc" {
-#      name       = "free5gc"
-#      repository = "${data.github_repository.free5gc.clone_url}"
-#      chart      = "free5gc"
-#    
-#      values = [
-#        "towards5gs-helm/charts/free5gc/values.yaml"
-#      ]
-#    }
+#provider "helm" {
+#  kubernetes {
+#    config_path    = "~/.kube/config"
+#    config_context = "${terraform.output.kubeconfig_context}"
+#  }
+#}
+#
+#data "github_repository" "free5gc" {
+#  owner = "5G-Grad-Proj"
+#  name  = "towards5gs-helm"
+#}
+#
+#resource "helm_release" "free5gc" {
+#  name       = "free5gc"
+#  repository = "${data.github_repository.free5gc.clone_url}"
+#  chart      = "free5gc"
+#
+#  values = [
+#    "towards5gs-helm/charts/free5gc/values.yaml"
+#  ]
+#}
+#
