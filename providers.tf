e1b8ba746fee5fd2601fe504a46ee674e4e16809
Ahmed <gomaaa447>
2023-03-03 21:56:10 +0200

    add helm provider

--- providers.tf
+++ providers.tf
@@ -16,3 +16,45 @@ provider "kubernetes" {
     args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
   }
 }
+
+################################################################################
+
+provider "helm" {
+  kubernetes {
+    config_path    = "~/.kube/config"
+    config_context = "5G-CoreNet"
+  }
+}
+
+
+resource "helm_release" "towards5gs" {
+  name       = "free5gc"
+  repository = "https://5g-grad-proj.github.io/towards5gs-helm/"
+  chart      = "free5gc"
+  version    = "0.1.0"
+
+   set {
+    name  = "image.repository"
+    value = "nginx"
+  }
+
+  set {
+    name  = "image.tag"
+    value = "1.19.9"
+  }
+  
+  set {
+    name  = "mongo.replicaSet.enabled"
+    value = "false"
+  }
+
+  set {
+    name  = "smf.service.type"
+    value = "NodePort"
+  }
+
+  set {
+    name  = "nrf.service.type"
+    value = "NodePort"
+  }
+}
